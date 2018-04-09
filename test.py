adb2b40a8d2b91970e2502441120227a5723c692
maumau <maumaumaumaumaumaumaumaumaumau@gmail.com>
2018-04-09 19:31:41 +0900

    update

--- test.py
+++ test.py
@@ -1,10 +1,16 @@
 import random
 from train_logger import TrainLogger
 
-tlog = TrainLogger("test_log", csv=True)
+tlog = TrainLogger("test_log", csv=True, use_tensorboradx=True, gspread_credential_path="credential.json", gspread_share_account="your_account@gmail.com", header=True, buff_size=3, notificate=True, suppress_err=False)
 
+# the header of data, it is required
 tlog.set_default_Keys(["num.", "val"])
 
-for i in range(1000):
+# set witch kind notification
+tlog.set_notificator(["slack"])
+# send notification using notificator
+tlog.notify("test")
+
+for i in range(5):
 	tlog.log([i, random.random()])
 
